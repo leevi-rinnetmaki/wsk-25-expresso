5992e618393e58a5ba23389cdcd1f66877de3d0d
leevi-rinnetmaki <leevi.rinnetmaki@hotmail.com>
2025-04-04 16:35:33 +0300

    uploading feature added

--- src/api/controllers/cat-controller.js
+++ src/api/controllers/cat-controller.js
@@ -14,7 +14,21 @@ const getCatById = (req, res) => {
 };
 
 const postCat = (req, res) => {
-  const result = addCat(req.body);
+  //const result = addCat(req.body);
+  const {originalname: cat_name, mimetype: owner, destination: birthdate, filename, size: weight} = req.file;
+  const kisu = {
+    cat_name,
+    owner,
+    birthdate,
+    filename,
+    weight,
+  };
+  const result = addCat(kisu);
+
+  console.log(1);
+  console.log(req.body);
+  console.log(req.file);
+  console.log(2);
   if (result.cat_id) {
     res.status(201);
     res.json({message: 'New cat added.', result});
